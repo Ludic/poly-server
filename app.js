ede72a2b5374b603a624ea240bb32ad2504e2c33
yintothayang <yintothayang@gmail.com>
2018-04-04 18:21:06 +0900

    fucking thing works

--- app.js
+++ app.js
@@ -57,11 +57,11 @@ methods.peerBAnswer = function(message, ws){
   })
 }
 
-methods.peerIceCandidate = function(message, ws){
+methods.peerCandidate = function(message, ws){
   wss.clients.forEach(function each(client) {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify({
-        method: 'peerIceCandidate',
+        method: 'peerCandidate',
         to: message.to,
         from: message.from,
         candidate: message.candidate,
@@ -122,107 +122,3 @@ wss.on('connection', function connection(ws) {
     lobbies: lobbies
   }))
 })
-
-
-
-// const express = require('express')
-// const http = require('http');
-// const WebSocket = require('ws')
-
-// const app = express()
-// const server = http.createServer(app)
-// const wss = new WebSocket.Server({ server, port: 3001 })
-
-// wss.on('connection', function connection(ws, req) {
-//   const location = url.parse(req.url, true);
-//   // You might use location.query.access_token to authenticate or share sessions
-//   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
-
-//   ws.send('something');
-// })
-
-// // List Lobbies
-// app.get('/lobbies', (req, res) => {
-//   res.setHeader('Content-Type', 'application/json')
-//   let response = JSON.stringify([
-//     {
-//       id: 5,
-//       creator: 6
-//     },
-//     {
-//       id: 3,
-//       owner: 2
-//     }
-//   ])
-
-//   res.send(response)
-// })
-
-// // Get a Lobby
-// app.get('/lobbies/:id', (req, res) => {
-//   res.setHeader('Content-Type', 'application/json')
-//   let response = JSON.stringify({
-//     id: req.params.id,
-//     creator: 6
-//   })
-
-//   res.send(response)
-// })
-
-// // Create a Lobby
-// app.post('/lobbies', (req, res) => {
-//   // TODO create the lobby
-//   console.log(req.params)
-
-//   res.setHeader('Content-Type', 'application/json')
-//   let response = JSON.stringify({
-//     id: req.params.id,
-//     creator: 6
-//   })
-
-//   res.send(response)
-// })
-
-
-// // Delete a Lobby
-// app.delete('/lobbies/:id', (req, res) => {
-//   // TODO create the lobby
-//   console.log(req.params)
-
-//   res.setHeader('Content-Type', 'application/json')
-//   let response = JSON.stringify({
-//     id: req.params.id,
-//     creator: 6
-//   })
-
-//   res.send(response)
-// })
-
-
-// // Join a Lobby
-// app.put('/lobbies/join', (req, res) => {
-//   // TODO join the lobby
-//   console.log(req.params)
-
-//   res.setHeader('Content-Type', 'application/json')
-//   let response = JSON.stringify({
-//     id: req.params.id,
-//     creator: 6
-//   })
-
-//   res.send(response)
-// })
-
-
-// // 404
-// app.get('*', function(req, res){
-//   res.send('no matching route', 404);
-// });
-
-// app.listen(8080, () => {
-//   console.log('Example app listening on port 8080!')
-// })
